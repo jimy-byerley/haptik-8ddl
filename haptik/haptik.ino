60e5ceffe4cc008f72047e92c55c40b009a3eb5f
jimy-byerley <jimy.byerley@gmail.com>
2019-12-21 18:59:17 +0100

    added sample periods for communication tasks

--- haptik/haptik.ino
+++ haptik/haptik.ino
@@ -7,8 +7,10 @@
 using namespace la;
 
 
+/// structure pour passer les informations de retour de force via le port-série
 struct ForceFeedback {
 	enum Type {
+		UNKNOWN,
 		NONE,
 		FORCE,
 		BLOCK
@@ -49,24 +51,29 @@ ForceFeedback receive_feedback() {
 	if (receive("\n", 1)) {
 		if 		(strcmp(recvbuff, "force") == 0) 	feedback.type = ForceFeedback::FORCE;
 		else if (strcmp(recvbuff, "block") == 0) 	feedback.type = ForceFeedback::BLOCK;
-		else										feedback.type = ForceFeedback::NONE;
+		else if (strcmp(recvbuff, "none") == 0)		feedback.type = ForceFeedback::NONE;
+		else										feedback.type = ForceFeedback::UNKNOWN;
+		if (feedback.type == ForceFeedback::NONE)
+			return feedback;
+		
 		size_t index = 0;
 		for (size_t i=0; i<8; i++) {
 			if (sscanf(recvbuff+index, "%f,", &feedback.vec(i)) != 1) {
-				feedback.type = ForceFeedback::NONE;
+				feedback.type = ForceFeedback::UNKNOWN;
 				break;
 			}
 			while (recvbuff[index] != ',' && index < recvsize)	index++;
 		}
 	}
-	else	feedback.type = ForceFeedback::NONE;
+	else	feedback.type = ForceFeedback::UNKNOWN;
 	return feedback;
 }
 
 
-bool enable_feedback = true;
-bool enable_measure = true;
-bool enable_assist = false;
+// drapeaux d'activation des fonctionnalités
+bool enable_feedback = true;	// retour de force
+bool enable_measure = true;		// envoi de la pose sur le port serie
+bool enable_assist = false;		// suivi de mouvement utilisateur (spasmes)
 
 HaptikDXL dxl;
 Delta model;
@@ -74,6 +81,10 @@ vec8 last_pose;
 ForceFeedback feedback;
 vec8 feedback_dir;
 vec8 feedback_origin;
+
+// nbr de périodes de loop entre chaque communications (envoi de pose et reception d'ordre)
+static const int comrefresh_sample = 10;
+int comrefresh = 0;	// compteur periodes depuis dernier envoi
 
 void setup() {
 	Serial.begin(57600);
@@ -110,47 +121,60 @@ void setup() {
 
 void loop() {
 	// restreindre les plages des moteurs (en attendant de faire ca avec des detections de singularités)
+	const float force_current = 1.;	// (Nm/mA)	TODO: a determiner experimentalement
+	const float current_corr = 5.;	// (mA/mm)	TODO: a affiner
+	
 	const float max_angle = 2;	// rad
 	const float min_angle = -0.5;	// rad
 	const float resist_current = 50;	// mA
 	vec8 angle;
-	vec8 current = vec8(0.);
 	
-	// get the pose and apply limitations
+	// get the pose
 	for (size_t i=0; i<N; i++) 	angle(i) = dxl.get_position(i);
+	
+	Delta::state pose = model.mgd_solve(angle, last_pose);	// compute the pose
+	
+	if (comrefresh == 0) {
+		if (enable_measure) 
+			send_pose(pose.X);	// send to computer
+		
+		if (enable_feedback) {
+			// receive order and setup execution datas
+			ForceFeedback new_fb = feedback = receive_feedback();
+			if (new_fb.type != ForceFeedback::UNKNOWN)	{
+				feedback = new_fb;
+				switch (feedback.type) {
+					case ForceFeedback::FORCE:
+						feedback_dir = force_current * (model.mci(pose) * feedback.vec);
+						break;
+					case ForceFeedback::BLOCK:
+						// le vecteur passé est une direction (donc vecteur normé), si sa norme n'est pas 1, elle servira de facteur a l'asservissement
+						feedback_dir = current_corr * model.mci(pose) * feedback.vec;
+						feedback_origin = pose.X;
+						break;
+				}
+			}
+		}
+	}
+	comrefresh = (comrefresh+1)%comrefresh_sample;
+	
+	// apply feedback
+	vec8 current;
+	switch (feedback.type) {
+		case ForceFeedback::FORCE:
+			current = feedback_dir;
+			break;
+		case ForceFeedback::BLOCK:
+			current = dot(pose.X - feedback_origin, feedback.vec) * feedback_dir;
+			break;
+		default:
+			current = vec8(0.);
+	}
+	
+	// apply limitations
 	for (size_t i=0; i<N; i++) {
 		if 		(angle(i) < min_angle)	current(i) = resist_current;
 		else if (angle(i) > max_angle)	current(i) = -resist_current;
-	}
-	
-	Delta::state pose = model.mgd_solve(angle, last_pose);	// compute the pose
-	send_pose(pose.X);	// send to computer
-	
-	if (enable_feedback) {
-		// receive order and setup execution datas
-		feedback = receive_feedback();
-		switch (feedback.type) {
-			case ForceFeedback::FORCE:
-				feedback_dir = model.mci(pose) * feedback.vec;
-				break;
-			case ForceFeedback::BLOCK:
-				// le vecteur passé est une direction (donc vecteur normé), si sa norme n'est pas 1, elle servira de facteur a l'asservissement
-				feedback_dir = model.mci(pose) * feedback.vec;
-				feedback_origin = pose.X;
-				break;
-		}
-		
-		// apply feedback
-		const float FORCE_CURRENT = 1.;	// (Nm/mA)	TODO: a determiner experimentalement
-		const float CURRENT_CORR = 5.;	// (mA/mm)	TODO: a affiner
-		switch (feedback.type) {
-			case ForceFeedback::FORCE:
-				current = current + FORCE_CURRENT*feedback_dir;
-				break;
-			case ForceFeedback::BLOCK:
-				current = current + CURRENT_CORR*dot(pose.X - feedback_origin, feedback.vec) * feedback_dir;
-				break;
-		}
 	}
 	
 	// apply torques to motors
